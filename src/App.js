efc7f60ae538b3f2275d0ca6efadf495dfe9fc82
bsanser <bsanser@gmail.com>
2023-04-27 12:22:47 +0100

    feat: search and highlight

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import algoliasearch from 'algoliasearch/lite';
+import { InstantSearch, SearchBox, Hits,  Highlight, RefinementList } from 'react-instantsearch-hooks-web';
 import './App.css';
+
+const searchClient = algoliasearch(process.env.REACT_APP_ALGOLIA_APP_ID, process.env.REACT_APP_ALGOLIA_SEARCH_API_KEY);
+
+function Hit({ hit }) {
+  return (
+    <article>
+      <img src={hit.image} alt={hit.name} />
+      <p>{hit.categories[0]}</p>
+      <h1>
+        <Highlight attribute="name" hit={hit}/>
+      </h1>
+      <p>${hit.price}</p>
+    </article>
+  );
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <InstantSearch searchClient={searchClient} indexName="phones" insights>
+      <SearchBox />
+      <RefinementList attribute="brand" />
+      <Hits hitComponent={Hit} />
+    </InstantSearch>
+      
   );
 }
+
 
 export default App;
